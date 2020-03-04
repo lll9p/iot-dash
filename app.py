a9e4ec3b94dc89da282b451d282e14d18d431093
lll9p <lll9p.china@gmail.com>
2020-03-05 01:18:26 +0800

    update

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import dash_html_components as html
 from model import Sensor
 
 sensor = Sensor()
-data = list(sensor.get_data_by_time("2020-02-23"))
-app = dash.Dash()
+data = list(sensor.get_data_by_time("2020-03-05"))
+app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.H1(children='传感器显示'),
+    html.H1(children='Sensor Monitor'),
 
     dcc.Graph(
         id='example-graph',
@@ -17,28 +17,30 @@ app.layout = html.Div(children=[
             'data': [
                 {'x': data[0],
                  'y': data[1],
-                 'mode':'markers',
-                 'name': '温度/℃'},
-            ],
-            'layout': {
-                'title': '温度显示'
-            }
-        }
-    ),
-    dcc.Graph(
-        id='example-graph2',
-        figure={
-            'data': [
+                 'mode':'line+markers',
+                 'name': 'Temperature'},
                 {'x': data[0],
                  'y': data[2],
-                 'mode':'markers',
-                 'name': '湿度/%'},
+                 'mode':'line+markers',
+                 'name': 'Humidity',
+                 'yaxis':'y2'},
             ],
             'layout': {
-                'title': '湿度显示'
-            }
+                'width': '0.5',
+                'height': '0.6',
+                'title': 'Room Conditions Monitor',
+                'xaxis': dict(title='DateTime'),
+                'yaxis': dict(
+                    title='Temperature/℃'),
+                'yaxis2': dict(
+                    title="Humidity/%",
+                    overlaying='y',
+                    side='right'
+                )
+            },
+            # 'render_mode': 'webgl',
         }
-    )
+    ),
 ])
 
 if __name__ == '__main__':
